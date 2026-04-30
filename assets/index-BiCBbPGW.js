99c747b20bb7d66fc0d64fe4cedc6d891a2e56f4
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 13:00:09 +0900

    Deploy: preload + canvas size + empty label

--- assets/index-BiCBbPGW.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BOIue3GQ.js","assets/index-C_ea5jl7.js","assets/index-DfLdwShF.css"])))=>i.map(i=>d[i]);
-import{L as r,_ as t}from"./index-C_ea5jl7.js";const o=r("Share",{web:()=>t(()=>import("./web-BOIue3GQ.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
---
+++ assets/index-BrbKr2D4.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C8-V28Te.js","assets/index-BalAIWUv.js","assets/index-BsXMjYUW.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-BalAIWUv.js";const o=r("Share",{web:()=>t(()=>import("./web-C8-V28Te.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
