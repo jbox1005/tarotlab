52861e7722c58e158590621b03f355dcdd123387
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 13:26:51 +0900

    Deploy: icon-only canvas controls

--- assets/index-DRzKmIIL.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-2j2kmZRC.js","assets/index-CQgIIwUP.js","assets/index-SQyvUDKu.css"])))=>i.map(i=>d[i]);
-import{L as r,_ as t}from"./index-CQgIIwUP.js";const o=r("Share",{web:()=>t(()=>import("./web-2j2kmZRC.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
