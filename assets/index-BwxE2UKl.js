3f1be2675ec9dc817e9c26b5ba967cbe55d76bbf
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 10:42:45 +0900

    Deploy: memoize spread layout outline+label

--- assets/index-BwxE2UKl.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bc2tSHKy.js","assets/index-BpvoEKG3.js","assets/index-BGVbjVuV.css"])))=>i.map(i=>d[i]);
-import{K as r,_ as t}from"./index-BpvoEKG3.js";const o=r("Share",{web:()=>t(()=>import("./web-Bc2tSHKy.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
