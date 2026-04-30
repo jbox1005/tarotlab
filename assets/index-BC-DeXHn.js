7c7fb39e06cb88ada69026a16a055eaeb39f20ca
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 14:12:08 +0900

    Deploy: spread preview pentagram/yes_no fix

--- assets/index-BC-DeXHn.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-E72hHHIr.js","assets/index-BqNPPnrx.js","assets/index-Cp0-16G-.css"])))=>i.map(i=>d[i]);
-import{L as r,_ as t}from"./index-BqNPPnrx.js";const o=r("Share",{web:()=>t(()=>import("./web-E72hHHIr.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
