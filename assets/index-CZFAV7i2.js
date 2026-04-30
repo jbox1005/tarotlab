74a5b67fdec7b6985582ecbc3b3b3a6b6469e6e0
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 11:55:24 +0900

    Deploy: red top delete button

---
+++ assets/index-CZFAV7i2.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Cq0r0dCV.js","assets/index-O1FXJO-z.js","assets/index-Cbet27TU.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-O1FXJO-z.js";const o=r("Share",{web:()=>t(()=>import("./web-Cq0r0dCV.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
