6c0166aa79bb5f0492384fa5d025557dbc962aab
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 12:09:35 +0900

    Deploy: C — SpreadLayout slot inline transform

---
+++ assets/index-DZGObuot.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DYA6W3zO.js","assets/index-BOMpeEEE.js","assets/index-DfLdwShF.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-BOMpeEEE.js";const o=r("Share",{web:()=>t(()=>import("./web-DYA6W3zO.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
