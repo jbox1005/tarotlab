a3d78b58da5bd8e986e99862f530effe11ec88ea
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 09:39:17 +0900

    Deploy: slower initial fade-in (0.5s)

---
+++ assets/index-BUTexKWp.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CK4HMsth.js","assets/index-LaWvAH8a.js","assets/index-DzZeWTIy.css"])))=>i.map(i=>d[i]);
+import{K as r,_ as t}from"./index-LaWvAH8a.js";const o=r("Share",{web:()=>t(()=>import("./web-CK4HMsth.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
