c9ed85bbe9c1ca75b810cd8f56592bccb6c47ec9
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 11:07:29 +0900

    Deploy: version info dialog with floating logo

--- assets/index-CCRHx6Zo.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-N1xLlY7U.js","assets/index-DXHGXDtS.js","assets/index-F-hwL0QC.css"])))=>i.map(i=>d[i]);
-import{L as r,_ as t}from"./index-DXHGXDtS.js";const o=r("Share",{web:()=>t(()=>import("./web-N1xLlY7U.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
