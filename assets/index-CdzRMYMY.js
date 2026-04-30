7ce1360ee3ba9b3ff6ac06faaba323e92b4be272
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 14:34:16 +0900

    Deploy: user spread canvas ratio (3 options)

---
+++ assets/index-CdzRMYMY.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DDoHqsYO.js","assets/index-BPm2ohkH.js","assets/index-ClMHlxm1.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-BPm2ohkH.js";const o=r("Share",{web:()=>t(()=>import("./web-DDoHqsYO.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
