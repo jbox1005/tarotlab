d16da747cef1036aa5d5cfdd8bf18414cd47cf26
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 10:57:06 +0900

    Deploy: spread editor apply/preview-toggle/top-save UX

---
+++ assets/index-ClNamK4o.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DzKbAURO.js","assets/index-B68sQVdI.js","assets/index-M2nIzmbl.css"])))=>i.map(i=>d[i]);
+import{K as r,_ as t}from"./index-B68sQVdI.js";const o=r("Share",{web:()=>t(()=>import("./web-DzKbAURO.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
