074e8d7a9804edb9d307f6ef47f9c26a426b0b43
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 13:58:29 +0900

    Deploy: spread preview no clip + scroll fallback

---
+++ assets/index-BGA9np5x.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-sAhrrx3b.js","assets/index-Da4zE6Qq.js","assets/index-2mUVts7h.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-Da4zE6Qq.js";const o=r("Share",{web:()=>t(()=>import("./web-sAhrrx3b.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
