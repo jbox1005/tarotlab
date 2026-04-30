62fa16349c131cfb91d44e85819de513591ab16e
jbox1005 <jbox1005@users.noreply.github.com>
2026-04-30 11:16:15 +0900

    Deploy: version row in accordion style

---
+++ assets/index-DkAZrcdU.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BsyLMQVf.js","assets/index-CT-lmzFI.js","assets/index-B5bkzvxO.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-CT-lmzFI.js";const o=r("Share",{web:()=>t(()=>import("./web-BsyLMQVf.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
