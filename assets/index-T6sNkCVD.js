d0d6766bb6a607ed200c5b54799cffe8679eb76c
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-04 18:41:21 +0900

    Deploy: Pre-reading Ritual mode (MVP)

---
+++ assets/index-T6sNkCVD.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BmEdfhKN.js","assets/index-CAopsxU3.js","assets/index-BAW8oTyk.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-CAopsxU3.js";const o=r("Share",{web:()=>t(()=>import("./web-BmEdfhKN.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
