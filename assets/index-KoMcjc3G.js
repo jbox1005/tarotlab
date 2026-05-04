e38625b538dbbefb9278f0606ceccce2e908421b
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-04 18:54:31 +0900

    Deploy: ritual mode v1.1 (default ON + dynamic messages + redesign)

---
+++ assets/index-KoMcjc3G.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BUv0Vy0M.js","assets/index-DT7HwGMX.js","assets/index-DzXw3PmR.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-DT7HwGMX.js";const o=r("Share",{web:()=>t(()=>import("./web-BUv0Vy0M.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
