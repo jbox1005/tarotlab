fe719fbeb28fb612432506f5d8d24308b3421dc9
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-04 19:32:24 +0900

    Deploy: ritual→reading fade transition

---
+++ assets/index-ezQOPsbB.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Ci7DvzKI.js","assets/index-6A4HvYiz.js","assets/index-CVJNM8H5.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-6A4HvYiz.js";const o=r("Share",{web:()=>t(()=>import("./web-Ci7DvzKI.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
