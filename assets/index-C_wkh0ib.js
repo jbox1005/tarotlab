06bb2c9bfcd0c9a94be237b435e92f53e54c5bc5
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-05 19:50:57 +0900

    Deploy: ritual guide phrasing clarified

---
+++ assets/index-C_wkh0ib.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-sYatAWQJ.js","assets/index-c5-5J4Yc.js","assets/index-BV9Vz3u6.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-c5-5J4Yc.js";const o=r("Share",{web:()=>t(()=>import("./web-sYatAWQJ.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
