da5a5dfddf93f3cfb4cb00aa31809525b4093c61
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-05 13:55:09 +0900

    Deploy: home logo+fan bigger, tagline shift tweak

---
+++ assets/index-ChsSfSwk.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B5B_Cnt0.js","assets/index-CmrWm5cY.js","assets/index-pmzJMIE9.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-CmrWm5cY.js";const o=r("Share",{web:()=>t(()=>import("./web-B5B_Cnt0.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
