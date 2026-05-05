ef0f7e036a6dceb86b0dd4f3b87992adde79b4af
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-05 11:52:14 +0900

    Deploy: remove gaze mode

---
+++ assets/index-C8WBmG51.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bvi0kMAw.js","assets/index-C2uuc3xq.js","assets/index-G6yPcnf8.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-C2uuc3xq.js";const o=r("Share",{web:()=>t(()=>import("./web-Bvi0kMAw.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
