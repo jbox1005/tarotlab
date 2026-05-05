fe4727b2106541a2b6e75c8381757001cd11aaa5
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-05 13:34:02 +0900

    Deploy: home preserve outer padding + trim inside

---
+++ assets/index-DmCEstwd.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-FmdJbWU1.js","assets/index-D9u0B62n.js","assets/index-DQ-0VB3j.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-D9u0B62n.js";const o=r("Share",{web:()=>t(()=>import("./web-FmdJbWU1.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
