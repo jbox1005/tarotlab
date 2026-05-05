5023ec0739de1e0843efd47119f186fd157a6a87
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-05 19:55:11 +0900

    Deploy: ritual guide phrasing polish

---
+++ assets/index-Dw6Xc7_i.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DAXVHzS_.js","assets/index-B4lO3riG.js","assets/index-BV9Vz3u6.css"])))=>i.map(i=>d[i]);
+import{L as r,_ as t}from"./index-B4lO3riG.js";const o=r("Share",{web:()=>t(()=>import("./web-DAXVHzS_.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
