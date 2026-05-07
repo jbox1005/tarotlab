0511db5ca4cc7d6de4be3a906b45922b5304dfa6
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-08 02:09:01 +0900

    Spread guide rename + 기타 section + button icons match parents

--- assets/index-BGWrUmt0.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CJb5_1n3.js","assets/index-DGDvziu2.js","assets/index-DxFhCm9k.css"])))=>i.map(i=>d[i]);
-import{Y as r,_ as t}from"./index-DGDvziu2.js";const o=r("Share",{web:()=>t(()=>import("./web-CJb5_1n3.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
---
+++ assets/index-DbqGOuMt.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DF90S7PP.js","assets/index-CxLE6hAD.js","assets/index-DxFhCm9k.css"])))=>i.map(i=>d[i]);
+import{Y as r,_ as t}from"./index-CxLE6hAD.js";const o=r("Share",{web:()=>t(()=>import("./web-DF90S7PP.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
