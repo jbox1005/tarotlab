1c72759aa7154215ea1ecffe58482be076904753
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-08 02:22:36 +0900

    Notices popups: full i18n

---
+++ assets/index-C7dWNqKJ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BCT5uw0N.js","assets/index-DLQC0HpK.js","assets/index-DgXOuw4E.css"])))=>i.map(i=>d[i]);
+import{Y as r,_ as t}from"./index-DLQC0HpK.js";const o=r("Share",{web:()=>t(()=>import("./web-BCT5uw0N.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
