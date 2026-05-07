c1797b945057af7b523d3cb68de21cdafa8e868a
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-07 19:31:22 +0900

    Notices: auto-dismiss after first view, larger token budget for long body translation, polish publish button

--- assets/index-CeU2gdTA.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BcJ1lX1M.js","assets/index-HvP4vOYN.js","assets/index-CYCTm-b8.css"])))=>i.map(i=>d[i]);
-import{O as r,_ as t}from"./index-HvP4vOYN.js";const o=r("Share",{web:()=>t(()=>import("./web-BcJ1lX1M.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
