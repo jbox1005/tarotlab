a6799e1f2de33a9448f54256fd9dc0f952d0af9a
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-07 19:11:57 +0900

    Notices system: remote-fetched banner + admin page with KO→4-lang AI translation + platform targeting + App Store/Play Store action buttons

--- assets/index-D-bteyfH.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-PZxND0Qo.js","assets/index-BumwAJiN.js","assets/index-Dp85XrXo.css"])))=>i.map(i=>d[i]);
-import{L as r,_ as t}from"./index-BumwAJiN.js";const o=r("Share",{web:()=>t(()=>import("./web-PZxND0Qo.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
