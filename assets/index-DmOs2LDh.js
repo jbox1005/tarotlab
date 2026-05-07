40afef399f87878e9e968ac0f1ce5bfbe2d03f9f
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-07 23:42:20 +0900

    Settings: notices diagnostic dialog (button next to version close) — see fetch URL/cache/dismissed/per-notice filter reason

--- assets/index-DmOs2LDh.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-By6m7I9f.js","assets/index-Vej5OjFl.js","assets/index-C_XfZ8ho.css"])))=>i.map(i=>d[i]);
-import{Y as r,_ as t}from"./index-Vej5OjFl.js";const o=r("Share",{web:()=>t(()=>import("./web-By6m7I9f.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
---
+++ assets/index-E-qCLH6T.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DqJs3gxG.js","assets/index-BRrbsPDr.js","assets/index-C_XfZ8ho.css"])))=>i.map(i=>d[i]);
+import{Y as r,_ as t}from"./index-BRrbsPDr.js";const o=r("Share",{web:()=>t(()=>import("./web-DqJs3gxG.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
