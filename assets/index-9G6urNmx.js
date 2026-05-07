d365a0799e54b5e5e74a71e0cc1017bdfa4c7c84
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-07 18:03:54 +0900

    Meditation: fix bottom buttons overlapping iOS home indicator + widget click sound split (toggle vs navigate)

--- assets/index-9G6urNmx.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CNWst1-r.js","assets/index-C42CeTdz.js","assets/index-BHJZhkKy.css"])))=>i.map(i=>d[i]);
-import{L as r,_ as t}from"./index-C42CeTdz.js";const o=r("Share",{web:()=>t(()=>import("./web-CNWst1-r.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
