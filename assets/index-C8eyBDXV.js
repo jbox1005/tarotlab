75bcad1ad879026e3d94d82881bf4ed52dcce2b2
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-08 00:09:03 +0900

    Widget marquee: pad-right inside copy + shift=copyW (no overshoot) + slower 22px/s

---
+++ assets/index-C8eyBDXV.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web--yopIjsg.js","assets/index-BPC-fV2g.js","assets/index-DM5Fl2nj.css"])))=>i.map(i=>d[i]);
+import{Y as r,_ as t}from"./index-BPC-fV2g.js";const o=r("Share",{web:()=>t(()=>import("./web--yopIjsg.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
