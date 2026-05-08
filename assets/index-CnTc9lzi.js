4032bd60e7a0cda3bc46dd02004979eb29000fe3
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-08 09:35:31 +0900

    Custom spread preset bullet consistency

--- assets/index-CnTc9lzi.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bl7nfhZl.js","assets/index-D7aKad5r.js","assets/index-D4YPbJS5.css"])))=>i.map(i=>d[i]);
-import{Y as r,_ as t}from"./index-D7aKad5r.js";const o=r("Share",{web:()=>t(()=>import("./web-Bl7nfhZl.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
---
+++ assets/index-NwaIi82H.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BT2F0tqi.js","assets/index-BVBcFHRP.js","assets/index-D4YPbJS5.css"])))=>i.map(i=>d[i]);
+import{Y as r,_ as t}from"./index-BVBcFHRP.js";const o=r("Share",{web:()=>t(()=>import("./web-BT2F0tqi.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
