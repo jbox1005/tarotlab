32dd25fb395481f0bf671803be6240dccf95556c
jbox1005 <jbox1005@users.noreply.github.com>
2026-05-10 00:43:39 +0900

    deploy: past segment advice card layout + label fix

---
+++ assets/index-D5PUXueG.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B-1o50L3.js","assets/index-t-kG-T75.js","assets/index-BBshEtIq.css"])))=>i.map(i=>d[i]);
+import{Y as r,_ as t}from"./index-t-kG-T75.js";const o=r("Share",{web:()=>t(()=>import("./web-B-1o50L3.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
